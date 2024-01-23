4baa6032fb4bfe8404cd3cfbad0bf03dba96302e
Juan Henao <juanhenao@Juans-MacBook-Pro.local>
2024-01-23 17:39:14 -0500

    working users, registration, session, etc, and crud company references to user

---
+++ app/models/company.rb
@@ -0,0 +1,10 @@
+class Company < ApplicationRecord
+  belongs_to :user
+
+  validates :name,
+           presence: true,
+           uniqueness: true
+  validates :address, :established_year,
+           presence: true
+
+end
